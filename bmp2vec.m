e5565776c4fe80e057690da5864d920c4e516ddb
Zhihan Ning <44255983+add-oiler@users.noreply.github.com>
2020-07-21 18:43:54 +0800

    Update bmp2vec.m

--- bmp2vec.m
+++ bmp2vec.m
@@ -1,46 +1,43 @@
+% 将align后的bmp文件转为可用pca降维的vec*n矩阵
+
 clear
 close all
 
 load hs
+c1_name = dir('ppi72\c1\*bmp');
+c6000_name = dir('ppi72\c6000\*bmp');
+total = numel(c1_name) + numel(c6000_name);
 
-% rgb = imread('DiabetesMellitus.bmp');
-rgb = imread('Healthy.bmp');
-shape = size(rgb);
-
-%{
-for i = 1:217
-    for j = 1:243
-        is_bg = 1;
-        for k = 1:3
-            if rgb(i,j,k) ~= 255
-                is_bg = 0;
-                break;
-            end
-        end
-        if is_bg == 1
-            for k = 1:3
-                hs(i,j,k) = 1;
+for p=1:total
+    fprintf('%.2f%%\n',100 * p / total);
+    if p <= numel(c1_name)
+        img = imread(['ppi72\c1\' c1_name(p).name]);
+    else
+        img = imread(['ppi72\c6000\' c6000_name(p - numel(c1_name)).name]);
+    end
+    shape = size(img);
+    res = zeros(1);
+    cnt = 0;
+    for k = 1:3
+        for i = 1:shape(1)
+            for j = 1:shape(2)
+                if hs(i,j) == 0
+                    cnt = cnt + 1;
+                    res(cnt) = img(i,j,k);
+                end
             end
         end
     end
-end
-%}
-
-res = zeros(1);
-cnt = 1;
-
-for k = 1:3
-    for i = 1:shape(1)
-        for j = 1:shape(2)
-            if hs(i,j) == 0
-                res(cnt) = rgb(i,j,k);
-                cnt = cnt + 1;
-            end
-        end
+    if p == 1
+        ppi72_c1 = res;
+    elseif (1 < p) && (p <= numel(c1_name))
+        ppi72_c1 = [ppi72_c1;res]; % How I wish I knew how to allocate memory.
+    elseif p == numel(c1_name) + 1
+        ppi72_c6000 = res;
+    else
+        ppi72_c6000 = [ppi72_c6000;res];
     end
 end
 
-                
-    
-            
-    
+save('ppi72_c1.mat','ppi72_c1','-v7.3');
+save('ppi72_c6000.mat','ppi72_c6000','-v7.3');
